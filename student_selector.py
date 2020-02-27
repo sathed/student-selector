02e7d46a76f53d73408bf65d4bb4cd2b4ad756d5
Clint <clint.pedersen@gmail.com>
2020-02-27 13:22:14 -0700

    Update student_selector.py
    Simplified things.

--- student_selector.py
+++ student_selector.py
@@ -3,40 +3,24 @@ from random import seed, randint
 import calendar
 import time
 
+# Get the number of seconds since epoch.
 seconds_since_epoch = calendar.timegm(time.gmtime())
 
 # These number correspond to the number in your AWS username.
-students = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-    "18",
-    "19",
-    "20",
-    "21",
-    "22",
-    "23"
-]
+students = []
+num_students = 23
+# Create a list of AWS username numbers
+for i in range(1, num_students+1):
+    students.append(str(i))
 
+# Seconds since epoch as the seed.
+# This will inject a new random seed every second.
 seed(seconds_since_epoch)
 
 # Winner #1
 random_number = randint(0, len(students) - 1)
 print("First winner is {}".format(students[random_number]))
+# Prevent duplicates.
 students.pop(random_number)
 
 # Winner #2
